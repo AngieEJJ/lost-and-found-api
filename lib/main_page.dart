6a2a432341f93303c0a04cdf38f277ce9cdc8b5a
eunjinjeong <eunjin.jeong.5115@gmail.com>
2024-01-10 22:16:05 +0900

    controller 사용하여 UI 기능까지 완성

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -10,17 +10,19 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   List result = [];
-  List filteredResult = [];
-  final TextEditingController controller = TextEditingController(); //선언
+  List filterdResult = [];
+  final TextEditingController controller = TextEditingController();
 
   @override
   void dispose() {
-    controller.dispose(); //사용완료 후 제거
+    controller.dispose();
     super.dispose();
   }
 
   @override
   void initState() {
+    // searchItem(""); //getData가 await으로 데이터를 가져옴 -> getData가 된 후에  result 리스트가 채워져야 필터링이 수행된다. searchItem에서 result 활용 가능.
+    // -> 여기에 넣으면 병렬진행임. -> 39번쨰 줄로 이동
     getData();
     super.initState();
   }
@@ -32,16 +34,20 @@ class _MainPageState extends State<MainPage> {
         'http://openapi.seoul.go.kr:8088/sample/json/lostArticleInfo/1/5/');
     // print(response.data['lostArticleInfo']['row'][0]['ID']);
 
-    setState(() {
+    // setState(() { // searchItem 내 setstate가 있으므로 중복 -> 불필요
       result = response.data['lostArticleInfo']['row'];
-
+      searchItem("");
       // print(result);
-    });
+      // print(result[0]);
+      // print(result.length);
+    // });
   }
 
-  void searchItem(String query) { //검색 함수 : query -> 매개변수
+  void searchItem(String query) {
     setState(() {
-      filteredResult = result.where((item) => item['GET_NAME'].toString().contains(query)).toList();
+      filterdResult = result
+          .where((item) => item['GET_NAME'].toString().contains(query))
+          .toList();
     });
   }
 
@@ -56,24 +62,29 @@ class _MainPageState extends State<MainPage> {
         child: Column(
           children: [
             TextField(
-              controller: controller, // 연결
-              onChanged: (value) {
-                searchItem(controller.text);
-                // print(controller.text);
-              },
-              decoration: const InputDecoration(
+              controller: controller,
+              // 연결
+              // onChanged: (value) { //
+              //   searchItem(controller.text);
+              //   // print(controller.text);
+              // },
+              decoration: InputDecoration(
                 labelText: '분실물을 입력하세요',
-                border: OutlineInputBorder(),
+                border: const OutlineInputBorder(),
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {
+                    searchItem(controller.text);
+                  },
+                ),
               ),
             ),
             const SizedBox(height: 10),
-            Container(
-              height: 300,
+            Expanded(
               child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: (filteredResult.isNotEmpty ? filteredResult.length : result.length),
+                  itemCount: filterdResult.length,
                   itemBuilder: (context, index) {
-                    var itemData = (filteredResult.isNotEmpty ? filteredResult[index] : result[index]);
+                    // print('분실물: ${result[index]}');
                     return Card(
                       child: Container(
                         height: 280,
@@ -83,11 +94,22 @@ class _MainPageState extends State<MainPage> {
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                          Text('분실물: ${itemData['GET_NAME']}', style: const TextStyle(fontSize: 20),),
-                          Text('상태: ${itemData['STATUS']}', style: const TextStyle(fontSize: 20),),
-                          Text('수령일자: ${itemData['GET_DATE']}', style: const TextStyle(fontSize: 20),),
-                          Text('수령장소: ${itemData['TAKE_PLACE']}', style: const TextStyle(fontSize: 20),
-                          ),
+                              Text(
+                                '분실물: ${filterdResult[index]['GET_NAME']}',
+                                style: const TextStyle(fontSize: 20),
+                              ),
+                              Text(
+                                '상태: ${filterdResult[index]['STATUS']}',
+                                style: const TextStyle(fontSize: 20),
+                              ),
+                              Text(
+                                '수령일자: ${filterdResult[index]['GET_DATE']}',
+                                style: const TextStyle(fontSize: 20),
+                              ),
+                              Text(
+                                '수령장소: ${filterdResult[index]['TAKE_PLACE']}',
+                                style: const TextStyle(fontSize: 20),
+                              ),
                             ],
                           ),
                         ),
